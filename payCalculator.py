ab3f4f6bdc70a7e06cb5dffe102ee60b4ab1af89
James Remus <James.Remus@veteransunited.com>
2022-01-25 16:44:36 -0600

    commented out try and except lines

--- payCalculator.py
+++ payCalculator.py
@@ -2,24 +2,24 @@ def calculatePay():
     
     # This first line is provided for you
     hrs = input("Enter Hours: ")
-    try:
-        float(hrs)
-    except:
-        print("Error, Please enter numeric input")
-        quit()
+    #try:
+       # float(hrs)
+    #except:
+       # print("Error, Please enter numeric input")
+       # quit()
     rate = input("Enter Rate: ")
-    try:
-        float(rate)
-    except:
-        print ("Error, Please enter numeric input")
-        quit()
+   # try:
+      #  float(rate)
+   # except:
+       # print ("Error, Please enter numeric input")
+        #quit()
     if float(hrs) > 40 :
         ot_pay = (float(hrs)- 40) * (float(rate)*1.5)
         reg_pay = float(40*float(rate))
         pay = ot_pay + reg_pay
     else:
         pay = float(hrs) * float(rate)
-    print ("Pay:" ,pay)
+    print ("Pay:", pay)
     # end assignment
 
 ## if you want to test locally before you try to sync

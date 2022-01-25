b23289e79c20a4f6330ac46b10a73963a1410ed3
James Remus <James.Remus@veteransunited.com>
2022-01-25 16:29:37 -0600

    added ot variable

--- payCalculator.py
+++ payCalculator.py
@@ -5,23 +5,25 @@ def calculatePay():
     try:
         float(hrs)
     except:
-        print("Error Please enter a number")
+        print("Error, Please enter numeric input")
         quit()
     rate = input("Enter Rate: ")
     try:
         float(rate)
     except:
-        print ("Error Please enter a number")
+        print ("Error, Please enter numeric input")
         quit()
     if float(hrs) > 40 :
-        pay = 40 * float(rate) + (float(hrs)- 40) * (float(rate)*1.5)
+        ot_pay = (float(hrs)- 40) * (float(rate)*1.5)
+        reg_pay = float(40*float(rate))
+        pay = ot_pay + reg_pay
     else:
         pay = float(hrs) * float(rate)
-    print ("Pay:", pay)
+    print ("Pay:" ,pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculatePay() and run > python payCalculator.py
 ## ***IMPORTANT*** please recomment before you submit/sync your assignment.
 ## OR YOUR TEST WILL NOT RUN
-# calculatePay()
+calculatePay()

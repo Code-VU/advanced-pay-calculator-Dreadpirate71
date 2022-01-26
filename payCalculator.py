63147a0f91b773ac92563284dc9b749c70473db8
James Remus <James.Remus@veteransunited.com>
2022-01-26 16:31:16 -0600

    changed Please to please in except comment

--- payCalculator.py
+++ payCalculator.py
@@ -16,9 +16,9 @@ def calculatePay():
                 pay = float(hrs) * float(rate)
             print ("Pay:", pay)
         except:
-            print ("Error, Please enter numeric input\n")
+            print ("Error,please enter numeric input\n")
     except:
-        print("Error, Please enter numeric input\n")
+        print("Error, please enter numeric input\n")
         
         
     
